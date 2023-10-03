53991c71221c134a17c6a6f352a1746610ed3b29
wand-CE <wanderson14117@gmail.com>
2023-10-02 22:08:21 -0400

    more changes on messages

--- mvc/view.py
+++ mvc/view.py
@@ -12,92 +12,220 @@ class Bot(customtkinter.CTk):
         super().__init__()
         self.controller = BotController(self)
         self.principal = customtkinter.CTkFrame(self)
-        self.principal.pack(fill = customtkinter.BOTH, expand=True)
+        self.principal.pack(fill=customtkinter.BOTH, expand=True)
         self.geometry('400x525')
         self.resizable(False, False)
         self.configure()
-        self.main_color = customtkinter.StringVar(value=self.controller.config['theme'])
+        self.main_color = customtkinter.StringVar(
+            value=self.controller.config['theme'])
         self.title('LinkedInAutoConnectBot')
         customtkinter.set_appearance_mode(self.main_color.get())
 
-
-        self.email_lab = customtkinter.CTkLabel(self.principal, text='Email:', font=('Arial', self.font_size))
+        self.email_lab = customtkinter.CTkLabel(
+            self.principal, text='Email:', font=('Arial', self.font_size))
         self.email_lab.place(x=self.position_x, y=37)
 
-        self.password_lab = customtkinter.CTkLabel(self.principal, text='Password:', font=('Arial', self.font_size))
+        self.password_lab = customtkinter.CTkLabel(
+            self.principal, text='Password:', font=('Arial', self.font_size))
         self.password_lab.place(x=self.position_x, y=102)
 
-        self.prof_lab = customtkinter.CTkLabel(self.principal, text='Profession to Search:', font=('Arial', self.font_size))
+        self.prof_lab = customtkinter.CTkLabel(
+            self.principal, text='Profession to Search:', font=('Arial', self.font_size))
         self.prof_lab.place(x=self.position_x, y=145)
 
-        self.message_lab = customtkinter.CTkLabel(self.principal, text='Custom Message to send: ', font=('Arial', self.font_size))
+        self.message_lab = customtkinter.CTkLabel(
+            self.principal, text='Custom Message to send: ', font=('Arial', self.font_size))
         self.message_lab.place(x=self.position_x, y=220)
 
-        self.connect_lab = customtkinter.CTkLabel(self.principal, text='Number of Connections: ', font=('Arial', self.font_size))
+        self.connect_lab = customtkinter.CTkLabel(
+            self.principal, text='Number of Connections: ', font=('Arial', self.font_size))
         self.connect_lab.place(x=self.position_x, y=420)
 
-
-        self.email = customtkinter.CTkEntry(self.principal, font=('Arial', self.font_size), placeholder_text='Your E-mail')
+        self.email = customtkinter.CTkEntry(self.principal, font=(
+            'Arial', self.font_size), placeholder_text='Your E-mail')
         self.email.place(x=self.position_x, y=60, w=375)
 
-        self.password = customtkinter.CTkEntry(self.principal, font=('Arial', self.font_size), show='*', placeholder_text='Your Password')
+        self.password = customtkinter.CTkEntry(self.principal, font=(
+            'Arial', self.font_size), show='*', placeholder_text='Your Password')
         self.password.place(x=90, y=102,  w=260)
 
         self.see_password = customtkinter.CTkButton(self.principal, font=('Arial', self.font_size), text='', fg_color='#ABABAB', hover_color='#6E6E6E',
-                                                    image=customtkinter.CTkImage(Image.open('images/eye.png')),
+                                                    image=customtkinter.CTkImage(
+                                                        Image.open('images/eye.png')),
                                                     command=lambda: [self.controller.show_password(self.password, self.see_password)])
         self.see_password.place(x=355, y=102,  w=35)
 
-        self.profession = customtkinter.CTkEntry(self.principal, font=('Arial', self.font_size))
+        self.profession = customtkinter.CTkEntry(
+            self.principal, font=('Arial', self.font_size))
         self.profession.place(x=self.position_x, y=177,  w=375)
 
-
-        self.message = customtkinter.CTkTextbox(self.principal, font=('Arial', self.font_size))
+        self.message = customtkinter.CTkTextbox(
+            self.principal, font=('Arial', self.font_size))
         self.message.place(x=self.position_x, y=245, h=165, w=375)
-
 
         self.start = customtkinter.CTkButton(self.principal, text='START', font=('Arial', self.font_size),
                                              command=lambda: self.controller.check_elements(self.email.get(),
                                                                                             self.password.get(),
                                                                                             self.profession.get(),
-                                                                                            self.message.get("0.0", "end"),
+                                                                                            self.message.get(
+                                                                                                "0.0", "end"),
                                                                                             self.optionmenu.get()))
         self.start.place(x=self.position_x, y=470, w=100)
 
         self.theme = customtkinter.CTkSwitch(self.principal, text="Dark Mode",
-                                command=lambda : [customtkinter.set_appearance_mode(self.main_color.get()),
-                                                   self.controller.change_theme(self.main_color.get())],
-                                variable=self.main_color, onvalue='dark', offvalue="light")
+                                             command=lambda: [customtkinter.set_appearance_mode(self.main_color.get()),
+                                                              self.controller.change_theme(self.main_color.get())],
+                                             variable=self.main_color, onvalue='dark', offvalue="light")
         self.theme.place(x=self.position_x, y=15)
 
-        self.optionmenu = customtkinter.CTkOptionMenu(self.principal, values=[f"{i}" for i in range(1, 51) if not i % 5])
+        self.optionmenu = customtkinter.CTkOptionMenu(
+            self.principal, values=[f"{i}" for i in range(1, 51) if not i % 5])
         self.optionmenu.place(x=self.position_x + 170, y=420, w=60)
 
         self.elements = (self.theme, self.email_lab, self.password_lab,
-                         self.email,self.password, self.prof_lab,
+                         self.email, self.password, self.prof_lab,
                          self.message_lab, self.connect_lab, self.start)
 
-        self.controller.change_language(self.controller.config['language'], self.elements)
+        self.controller.change_language(
+            self.controller.config['language'], self.elements)
 
         self.language = customtkinter.CTkOptionMenu(self.principal, values=sorted(["pt-BR", "en-US"]),
                                                     command=lambda _: [self.controller.change_language(self.language.get(), self.elements)])
         self.language.set(self.controller.config['language'])
-        self.language.place(x=self.position_x + 180, y=15, w=80)        
+        self.language.place(x=self.position_x + 180, y=15, w=80)
 
         self.connection_screen()
         self.mainloop()
-        
 
     def connection_screen(self):
         janela_secundaria = customtkinter.CTkToplevel(self)
+        # print(help(customtkinter.CTkToplevel))
         janela_secundaria.title("Connections")
-        janela_secundaria.geometry('300x300')
+        janela_secundaria.geometry('400x400')
+
         janela_secundaria.grab_set()
-        message = customtkinter.CTkTextbox(janela_secundaria, font=('Arial', self.font_size))
-        message.place(x=15, y=15)
+        message = customtkinter.CTkTextbox(
+            janela_secundaria, font=('Arial', self.font_size))
+        message.pack( padx=10, pady=5, fill=customtkinter.BOTH, expand=1)
+        stop = customtkinter.CTkButton(janela_secundaria, text='STOP', font=('Arial', self.font_size), fg_color="red",
+                                       hover_color="darkred", command=lambda: [self.controller.loggin_out()])
+        stop.pack(padx=10, pady=5)
         self.update_message(message)
-    
+
     def update_message(self, message):
         message.configure(state='normal')
-        message.insert(customtkinter.END ,'texto simples')
+        message.insert(customtkinter.END, 'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples'
+                       'texto simples')
         message.configure(state='disabled')
